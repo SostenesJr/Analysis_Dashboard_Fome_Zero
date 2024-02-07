b9a06f04c97f807483e8bbfefd100cc407282acd
SostenesJr <118483399+SostenesJr@users.noreply.github.com>
2024-02-07 17:49:42 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -23,7 +23,7 @@ st.sidebar.image(image, width =240)
 
 st.sidebar.header(":orange[Fome Zero]")
 
-st.sidebar.markdown('## :orange[Projeto de Análise de dados]')
+st.sidebar.markdown('## :orange[Projeto de Análise de Dados]')
 
 st.sidebar.markdown("""---""")
 
@@ -34,18 +34,18 @@ st.write("# Análise detalhada sobre os dados da companhia fome Zero")
 
 st.markdown(
     """
-    ### :orange[O Painel analítico Fome Zero foi construido para acamponhar as metricas de culinarias por regiâo, pais, cidade e restaurantes:]
+    ### :orange[O Painel analítico Fome Zero foi construido para acamponhar as metricas de culinárias por regiâo, país, cidade e restaurantes:]
     ####  :orange[Como utilizar esses Painel de avaliações?]
     #### - :orange[Visão Geral:]
         - Analíse feita de modo geral onde aborda informação importantes de modo amplo.  
     #### - :orange[Visão Pais:]
-        - Analíse feita de modo espefico quantidade, qualidade, preço e avaliações por cada pais.
+        - Analíse feita de modo espcíefico quantidade, qualidade, preço e avaliações por cada país.
     #### - :orange[Visão Cidade:]
-        - Analíse feita de modo espefico quantidade, qualidade, preço e avaliações por cada cidade.
+        - Analíse feita de modo específico quantidade, qualidade, preço e avaliações por cada cidade.
     #### - :orange[Visão Restaurantes:]
-        - Analíse feita de modo espefico quantidade, qualidade, preço e avaliações por cada restaurante.
-    #### - :orange[Visão Culinaria:]
-        - Analíse feita de modo espefico quantidade, qualidade, preço e avaliações por cada tipo de culinaria.
+        - Analíse feita de modo específico quantidade, qualidade, preço e avaliações por cada restaurante.
+    #### - :orange[Visão Culinária:]
+        - Analíse feita de modo específico quantidade, qualidade, preço e avaliações por cada tipo de culinária.
     
       
     #### :orange[Ask for Help]
